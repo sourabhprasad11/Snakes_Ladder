a5126bedc3cbd68f5cc2478f75ceeb00175865d7
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-11-18 20:18:50 +0530

    Added: player at position 0

--- src/main/java/com/snakesladder/App.java
+++ src/main/java/com/snakesladder/App.java
@@ -1,13 +1,11 @@
 package com.snakesladder;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.out.println( "Welcome to the Ladder-Snakes Game" );
+        Player player= new Player();
+    	player.position.add(0);
     }
 }
---
+++ src/main/java/com/snakesladder/Player.java
@@ -0,0 +1,8 @@
+package com.snakesladder;
+
+import java.util.ArrayList;
+
+public class Player {
+	
+	ArrayList<Integer> position= new ArrayList<Integer>();
+}
