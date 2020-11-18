ee019fbf4299bd35af202a5bf4843e3c36a69bf9
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-11-18 20:37:55 +0530

    Added: number generation between 1 to 6

--- src/main/java/com/snakesladder/App.java
+++ src/main/java/com/snakesladder/App.java
@@ -7,5 +7,6 @@ public class App
     	System.out.println( "Welcome to the Ladder-Snakes Game" );
         Player player= new Player();
     	player.position.add(0);
+    	int number=player.RollTheDice();
     }
 }
--- src/main/java/com/snakesladder/Player.java
+++ src/main/java/com/snakesladder/Player.java
@@ -5,4 +5,9 @@ import java.util.ArrayList;
 public class Player {
 	
 	ArrayList<Integer> position= new ArrayList<Integer>();
+	
+	int RollTheDice() {
+		int number= (int)(Math.floor(Math.random()*10)%6+1);
+		return number;
+	}
 }
