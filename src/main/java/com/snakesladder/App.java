e2758124cb4e3631392c94df867bf6d5d61d4c04
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-11-18 23:00:11 +0530

    Added: functionality for checking the options for the game

--- src/main/java/com/snakesladder/App.java
+++ src/main/java/com/snakesladder/App.java
@@ -2,11 +2,17 @@ package com.snakesladder;
 
 public class App 
 {
+
     public static void main( String[] args )
     {
     	System.out.println( "Welcome to the Ladder-Snakes Game" );
         Player player= new Player();
     	player.position.add(0);
     	int number=player.RollTheDice();
+    	System.out.println(number);
+    	
+    	int diceValue=player.OptionCheck(number);
+    	player.position.add(diceValue);
+    	System.out.println(player.position);
     }
 }
--- src/main/java/com/snakesladder/Player.java
+++ src/main/java/com/snakesladder/Player.java
@@ -5,9 +5,32 @@ import java.util.ArrayList;
 public class Player {
 	
 	ArrayList<Integer> position= new ArrayList<Integer>();
-	
+
 	int RollTheDice() {
 		int number= (int)(Math.floor(Math.random()*10)%6+1);
 		return number;
 	}
+	
+	int OptionCheck(int number) {
+		int option= (int)(Math.floor(Math.random()*10)%3+1);
+		Player playernumber= new Player();
+		switch(option) {
+		case 1: 
+			System.out.println( "No change in the position" );
+			number=0;
+			break;
+		case 2:
+			System.out.println( "Ladder Comes" );
+			number=number+0;
+			break;
+		case 3:
+			System.out.println( "Snake Comes" );
+			number=0-number;
+			break;
+		default:
+			System.out.println( "Error: Value not found" );
+		}
+		return number;
+	}
+	
 }
